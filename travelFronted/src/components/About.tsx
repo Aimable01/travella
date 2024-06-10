5a399df74e7f0d8f278d9c0d334da32915d6e0fe
= <=>
2024-06-10 07:42:34 +0200

    about page

---
+++ travelFronted/src/components/About.tsx
@@ -0,0 +1,38 @@
+const About = () => {
+  return (
+    <div className="px-10 py-5 md:px-24 md:py-10">
+      <h1 className="font-bold text-lg my-4">
+        Explore the World Travel Agency
+      </h1>
+      <p className="text-[15px] font-semibold text-gray-800">
+        Explore the World Ltd is your trusted global travel partner. We
+        specialize in holiday packages, flight bookings, hotel reservations, and
+        custom tours. Whether you're planning a solo adventure, a romantic
+        getaway, a family vacation, or a group trip, we have something for
+        everyone. <br />
+        <br />
+        Our Services Include: <br /> Holiday Packages: All-inclusive deals to
+        top destinations. <br /> Flight Bookings: Affordable fares for domestic
+        and international flights. <br /> Hotel Reservations: From budget to
+        luxury accommodations. <br /> Custom Tours: Personalized itineraries to
+        match your preferences. <br /> Cruises: Exclusive cruise packages.{" "}
+        <br /> Adventure Trips: Activities like hiking, scuba diving, and
+        safaris. <br /> Cultural Tours: Experience the rich heritage of various
+        destinations. <br />
+        <br />
+        Each listing features detailed descriptions, photos, and destination
+        information, making planning your trip easy. You can filter options by
+        destination, type of trip, price, duration, and more. <br />
+        <br /> Why Choose Us? Expert Travel Planners: Personalized service and
+        expert advice. Global Reach: Travel options to destinations worldwide.
+        Convenience: Easy booking and 24/7 customer support. <br />
+        <br /> Value: Competitive prices and exclusive deals. Explore the World
+        also offers property management solutions for vacation rentals, ensuring
+        a stress-free experience for property owners. Contact Explore the World
+        Ltd today to start planning your next unforgettable adventure!
+      </p>
+    </div>
+  );
+};
+
+export default About;
